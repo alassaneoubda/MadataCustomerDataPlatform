64ce183581f84c7fe6841dc7bfc9ed018187adf6
gucal <gucalaliriza@outlook.com>
2023-01-15 17:08:37 +0300

    Add banking dashboard

--- src/app/demo/components/dashboards/banking/dashboardbanking.component.ts
+++ src/app/demo/components/dashboards/banking/dashboardbanking.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SelectItem } from 'primeng/api';
+import { Subscription } from 'rxjs';
 import { Product } from 'src/app/demo/api/product';
 import { ProductService } from 'src/app/demo/service/product.service';
+import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
     templateUrl: './dashboardbanking.component.html',
@@ -9,12 +11,23 @@ import { ProductService } from 'src/app/demo/service/product.service';
 export class DashboardBankingComponent implements OnInit {
 
     products: Product[] = [];
-    
+
     dropdownItem: SelectItem[] = [];
 
     selectedDropdownItem: any;
 
-    constructor(private productService : ProductService) { }
+    visitorChart: any;
+
+    visitorChartOptions: any;
+
+    subscription!: Subscription
+
+
+    constructor(private productService: ProductService, private layoutService: LayoutService) {
+        this.subscription = layoutService.configUpdate$.subscribe(config =>{
+            this.initChart()
+        })
+     }
 
     ngOnInit() {
         this.productService.getProducts().then(data => this.products = data);
@@ -22,5 +35,58 @@ export class DashboardBankingComponent implements OnInit {
         this.dropdownItem.push({ label: 'Xbox Series X', value: { id: 1, name: 'Xbox One', code: 'XO' } });
         this.dropdownItem.push({ label: 'PlayStation 5', value: { id: 2, name: 'PS4', code: 'PS4' } });
         this.dropdownItem.push({ label: 'Nintendo Switch', value: { id: 3, name: 'Wii U', code: 'WU' } });
+
+        this.initChart();
+
+    }
+
+    initChart() {
+        const textColor = getComputedStyle(document.body).getPropertyValue('--text-color')
+        const surfaceLight = getComputedStyle(document.body).getPropertyValue('--surface-100')
+
+        this.visitorChart = {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+            datasets: [
+                {
+                    data: [600, 671, 660, 665, 700, 610, 810, 790, 710, 860, 810, 780],
+                    backgroundColor: '#6f42c1',
+                    fill: true,
+                    barPercentage: 0.75,
+                    stepped: true
+                }
+            ]
+        };
+
+        this.visitorChartOptions = {
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            responsive: true,
+            hover: {
+                mode: 'index'
+            },
+            scales: {
+                y: {
+                    min: 500,
+                    max: 900,
+                    ticks: {
+                        color: textColor
+                    },
+                    grid: {
+                        color: surfaceLight
+                    }
+                },
+                x: {
+                    ticks: {
+                        color: textColor
+                    },
+                    grid: {
+                        display: false
+                    }
+                }
+            }
+        };
     }
 }
