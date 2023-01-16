adcea0b8be6c90a31e8c4789da91df9b68996b20
gucal <gucalaliriza@outlook.com>
2023-01-16 11:05:01 +0300

    Add accessdenied2 page

--- src/app/demo/components/auth/auth-routing.module.ts
+++ src/app/demo/components/auth/auth-routing.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
         { path: 'error', loadChildren: () => import('./error/error.module').then(m => m.ErrorModule) },
         { path: 'error2', loadChildren: () => import('./error2/error2.module').then(m => m.Error2Module) },
         { path: 'access', loadChildren: () => import('./accessdenied/accessdenied.module').then(m => m.AccessdeniedModule) },
+        { path: 'access2', loadChildren: () => import('./accessdenied2/accessdenied2.module').then(m => m.Accessdenied2Module) },
         { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
         { path: 'login2', loadChildren: () => import('./login2/login2.module').then(m => m.Login2Module) },
         { path: '**', redirectTo: '/notfound' }
