000290fc854ed71faa38fd0f228710ee0859d133
gucal <gucalaliriza@outlook.com>
2023-01-16 10:51:03 +0300

    Add error2 page

--- src/app/demo/components/auth/auth-routing.module.ts
+++ src/app/demo/components/auth/auth-routing.module.ts
@@ -4,8 +4,10 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [RouterModule.forChild([
         { path: 'error', loadChildren: () => import('./error/error.module').then(m => m.ErrorModule) },
+        { path: 'error2', loadChildren: () => import('./error2/error2.module').then(m => m.Error2Module) },
         { path: 'access', loadChildren: () => import('./accessdenied/accessdenied.module').then(m => m.AccessdeniedModule) },
         { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+        { path: 'login2', loadChildren: () => import('./login2/login2.module').then(m => m.Login2Module) },
         { path: '**', redirectTo: '/notfound' }
     ])],
     exports: [RouterModule]
