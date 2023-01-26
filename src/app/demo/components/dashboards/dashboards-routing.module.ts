7c733d5aaac4426a80dc1e1a3c1ab65b27cac3b9
Onur Senture <woof.uyelik@gmail.com>
2023-01-26 10:37:32 +0300

    Fix typo

--- src/app/demo/components/dashboards/dashboards-routing.module.ts
+++ src/app/demo/components/dashboards/dashboards-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [RouterModule.forChild([
-        { path: '', data: {breadcrumb: 'E-commerce Dashboard'}, loadChildren: () => import('./e-commerce/dashboardecommerce.module').then(m => m.DashboardEcommerceModule) },
+        { path: '', data: {breadcrumb: 'E-Commerce Dashboard'}, loadChildren: () => import('./e-commerce/dashboardecommerce.module').then(m => m.DashboardEcommerceModule) },
         { path: 'dashboard-banking', data: {breadcrumb: 'Banking Dashboard'}, loadChildren: () => import('./banking/dashboardbanking.module').then(m => m.DashboardBankingModule) },
     ])],
     exports: [RouterModule]
