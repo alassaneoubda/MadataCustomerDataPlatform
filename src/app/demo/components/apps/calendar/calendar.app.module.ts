09cad1d45d33cb2311dede8b0d3e8349cdc9e303
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-02-24 16:10:55 +0300

    fixed apps pages needs retouching. The card styles are wrong #59

--- src/app/demo/components/apps/calendar/calendar.app.module.ts
+++ src/app/demo/components/apps/calendar/calendar.app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CalendarAppRoutingModule } from './calendar.app-routing.module';
@@ -13,12 +13,12 @@ import { DropdownModule } from 'primeng/dropdown';
 import { ToastModule } from 'primeng/toast'
 import { EventService } from 'src/app/demo/service/event.service';
 import { FullCalendarModule } from '@fullcalendar/angular';
-
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         CalendarAppRoutingModule,
+        FullCalendarModule,
         DialogModule,
         InputTextareaModule,
         ButtonModule,
@@ -26,8 +26,7 @@ import { FullCalendarModule } from '@fullcalendar/angular';
         InputTextModule,
         DropdownModule,
         ToastModule,
-        RippleModule,
-        FullCalendarModule
+        RippleModule
     ],
     declarations: [CalendarAppComponent],
     providers: [EventService]
